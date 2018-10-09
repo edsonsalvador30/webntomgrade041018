722860002865d086f38bdb3cbff36fcc0d223c2a
Edson Junior <edson.fonseca@techne.com.br>
2018-10-09 01:08:23 +0000

    Commitando sem marcar os arquivo que foram excluidos.

--- src/main/java/blockly/Bloco.java
+++ src/main/java/blockly/Bloco.java
@@ -4,24 +4,29 @@ import cronapi.*;
 import cronapi.rest.security.CronappSecurity;
 import java.util.concurrent.Callable;
 
+
 @CronapiMetaData(type = "blockly")
 @CronappSecurity
 public class Bloco {
 
-	public static final int TIMEOUT = 300;
+public static final int TIMEOUT = 300;
 
-	/**
-	 *
-	 * @return Var
-	 */
-	// Bloco
-	public static Var Executar() throws Exception {
-		return new Callable<Var>() {
+/**
+ *
+ * @return Var
+ */
+// Bloco
+public static Var Executar() throws Exception {
+ return new Callable<Var>() {
 
-			public Var call() throws Exception {
-				return cronapi.database.Operations.query(Var.VAR_NULL, Var.VAR_NULL);
-			}
-		}.call();
-	}
+   private Var item = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+    item = cronapi.database.Operations.query(Var.valueOf("app.entity.Pedido"),Var.valueOf("select p from Pedido p"));
+    return cronapi.database.Operations.getField(item, Var.VAR_NULL);
+   }
+ }.call();
+}
 
 }
+
